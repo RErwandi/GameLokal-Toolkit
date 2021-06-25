d694e5fb5e2b2f064fe12c1dc307cec4b69deb57
Reynold Erwandi <reynolderwandi@gmail.com>
2021-06-25 11:54:23 +0700

    v1.1.0 Refactor State Machine

---
+++ Assets/com.gamelokal.toolkit/Runtime/Patterns/StateMachine/State.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameLokal.Toolkit.Pattern
+{
+    public class State : MonoBehaviour
+    {
+        public string StateName => gameObject.name;
+
+        public UnityEvent onStateEnter;
+        public UnityEvent onStateExit;
+    }
+}
