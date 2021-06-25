4a00d71241abf052398d7fe0c237205e89fba62b
Reynold Erwandi <reynolderwandi@gmail.com>
2021-06-25 11:54:23 +0700

    Revert "v1.1.0 Refactor State Machine"
    This reverts commit d694e5fb5e2b2f064fe12c1dc307cec4b69deb57.

--- Assets/com.gamelokal.toolkit/Runtime/Patterns/StateMachine/StateMachine.cs
+++ Assets/com.gamelokal.toolkit/Runtime/Patterns/StateMachine/StateMachine.cs
@@ -1,84 +1,77 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Sirenix.OdinInspector;
 using UnityEngine;
+using System;
 
 namespace GameLokal.Toolkit.Pattern
 {
-    public class StateMachine : MonoBehaviour
-    {
-        [ValueDropdown("AvailableStates"), HorizontalGroup("TestState"), ShowInInspector, DisableInEditorMode, PropertyOrder(10)]
-        private string testState = "";
+	public struct StateChangeEvent<T> where T: struct, IComparable, IConvertible, IFormattable
+	{
+		public GameObject target;
+		public StateMachine<T> targetStateMachine;
+		public T newState;
+		public T previousState;
+
+		public StateChangeEvent(StateMachine<T> stateMachine)
+		{
+			target = stateMachine.target;
+			targetStateMachine = stateMachine;
+			newState = stateMachine.CurrentState;
+			previousState = stateMachine.PreviousState;
+		}
+	}
+	
+	public interface IStateMachine
+	{
+		bool TriggerEvents { get; set; }
+	}
+	
+	public class StateMachine<T> : IStateMachine where T : struct, IComparable, IConvertible, IFormattable
+	{
+		public GameObject target;
+		
+		public bool TriggerEvents { get; set; }
+		public T CurrentState { get; protected set; }
+		public T PreviousState { get; protected set; }
+
+        public delegate void OnStateChangeDelegate();
+        public OnStateChangeDelegate onStateChange;
         
-        [ValueDropdown("AvailableStates"), Required]
-        public string defaultState;
-        public State[] states = new State[0];
+        public StateMachine(GameObject target, bool triggerEvents)
+		{
+			this.target = target;
+			this.TriggerEvents = triggerEvents;
+		} 
         
-        public State CurrentState { get; private set; }
-        
-        [Button(ButtonSizes.Large), HideInPlayMode]
-        private void AddNewState()
-        {
-            var newState = new GameObject($"State {states.Length}");
-            var state = newState.AddComponent<State>();
-            newState.transform.parent = transform;
-            newState.transform.localPosition = Vector3.zero;
-            newState.transform.localRotation = Quaternion.identity;
-            newState.transform.localScale = Vector3.one;
-            states = states.Concat(new[] { state }).ToArray();
+		public virtual void ChangeState(T newState)
+		{
+			// if the "new state" is the current one, we do nothing and exit
+			if (newState.Equals(CurrentState))
+			{
+				return;
+			}
 
-            if (CurrentState == null)
-                CurrentState = state;
-        }
+			// we store our previous character movement state
+			PreviousState = CurrentState;
+			CurrentState = newState;
 
-        [Button(ButtonSizes.Small), HorizontalGroup("TestState"), DisableInEditorMode, PropertyOrder(11)]
-        private void ChangeState()
-        {
-            SetState(testState);
-        }
+            onStateChange?.Invoke();
 
-        private void Start()
-        {
-            foreach (var state in states)
-            {
-                if(state.gameObject.activeSelf)
-                    state.gameObject.SetActive(false);
-            }
-            
-            SetState(defaultState);
-        }
+            if (TriggerEvents)
+			{
+				EventManager.TriggerEvent (new StateChangeEvent<T> (this));
+			}
+		}
+		
+		public virtual void RestorePreviousState()
+		{
+			// we restore our previous state
+			CurrentState = PreviousState;
 
-        private List<string> AvailableStates
-        {
-            get
-            {
-                return states.Select(state => state.StateName).ToList();
-            }
-        }
+            onStateChange?.Invoke();
 
-        /// <summary>
-        /// Change state. this will call OnStateExit on the current state before calling OnStateEnter on the target state.
-        /// </summary>
-        /// <param name="stateName">Name of the state</param>
-        public void SetState(string stateName)
-        {
-            var newState = states.FirstOrDefault(o => o.StateName == stateName);
-
-            if(newState != null)
-            {
-                if (CurrentState != null)
-                {
-                    CurrentState.onStateExit?.Invoke();
-                    CurrentState.gameObject.SetActive(false);
-                }
-
-                newState.gameObject.SetActive(true);
-                CurrentState = newState;
-                CurrentState.onStateEnter?.Invoke();
-            }
-            else
-                Debug.Log($"{gameObject.name} : Trying to set unknown state {stateName}");
-        }
-    }
+            if (TriggerEvents)
+			{
+				EventManager.TriggerEvent (new StateChangeEvent<T> (this));
+			}
+		}	
+	}
 }
-
