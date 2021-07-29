527f12d3411123a4b9f162f7109c862f41356816
Reynold Erwandi <reynolderwandi@gmail.com>
2021-07-29 10:32:12 +0700

    Add CsvToScriptableObject

---
+++ Runtime/Tools/CsvToScriptableObject/Examples/CsvTestClass.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(order = 10, fileName = "Csv ScriptableObject", menuName = "Joy-CSV/Csv ScriptableObject")]
+public class CsvTestClass : CsvScriptableObject<CsvTestData>
+{
+    
+}
---
+++ Runtime/Tools/CsvToScriptableObject/Examples/CsvTestData.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+[System.Serializable]
+public class CsvTestData
+{
+    public string firstName;
+    public string lastName;
+    public int day;
+    public int month;
+    public int year;
+    public float price;
+    public int[] friendList;
+    [PreviewField]
+    public Sprite icon;
+}
