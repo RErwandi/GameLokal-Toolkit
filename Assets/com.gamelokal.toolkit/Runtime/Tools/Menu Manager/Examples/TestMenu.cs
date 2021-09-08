6471d9bdef84af6917dc33e0705ce26606396926
Reynold Erwandi <reynolderwandi@gmail.com>
2021-09-08 14:46:51 +0700

    Add example menu

---
+++ Assets/com.gamelokal.toolkit/Runtime/Tools/Menu Manager/Examples/TestMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameLokal.Toolkit
+{
+    public class TestMenu : Menu<TestMenu>
+    {
+        
+    }
+}
