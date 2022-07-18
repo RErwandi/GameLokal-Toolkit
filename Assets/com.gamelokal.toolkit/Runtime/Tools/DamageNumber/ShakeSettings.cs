c4ec871642014a7286f57e86cc2ae5fb15012f87
reynold <reynolderwandi@gmail.com>
2022-07-18 11:26:51 +0700

    Add Damage Number

---
+++ Assets/com.gamelokal.toolkit/Runtime/Tools/DamageNumber/ShakeSettings.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GameLokal.Toolkit
+{
+    [System.Serializable]
+    public struct ShakeSettings
+    {
+        public ShakeSettings(Vector2 customDefault)
+        {
+            offset = customDefault;
+            frequency = 50f;
+        }
+
+        [Tooltip("Moves back and fourth from negative offset to positive offset.")]
+        public Vector2 offset;
+        [Tooltip("Changes the speed at which the number moves back and fourth.\nUsed in a sinus function.")]
+        public float frequency;
+    }
+
+}
