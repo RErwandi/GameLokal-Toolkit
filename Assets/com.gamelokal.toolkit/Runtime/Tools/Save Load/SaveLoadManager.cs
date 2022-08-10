e5e1c4dfb6cfd48f4d19d06f7f1e468acf8b1f10
reynold <reynolderwandi@gmail.com>
2022-08-10 23:10:02 +0700

    Improve saving/loading with caching system

--- Assets/com.gamelokal.toolkit/Runtime/Tools/Save Load/SaveLoadManager.cs
+++ Assets/com.gamelokal.toolkit/Runtime/Tools/Save Load/SaveLoadManager.cs
@@ -2,7 +2,6 @@
 #if UNITY_EDITOR
 using System.Diagnostics;
 #endif
-using System.Linq;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
@@ -11,66 +10,38 @@ namespace GameLokal.Toolkit
 {
     public class SaveLoadManager : Singleton<SaveLoadManager>
     {
-        [Title("Configurations")]
-        public bool useEasySave;
-        [HideIf("useEasySave")]
-        public string defaultFilename = "gamelokal_savedata";
+        [InfoBox("If set to false, you need to call StoreCachedFile() to write save data in memory to permanent local file")]
+        public bool storeCachedFileAfterSaving = true;
         public bool saveOnApplicationPause = true;
         public bool saveOnApplicationQuit = true;
         public bool useAutoSave = true;
         [ShowIf("useAutoSave"), Range(1, 100), SuffixLabel("minute(s)")]
         public float autoSaveInterval = 5;
-        
-        /// <summary>
-        /// Used to remember filename that is currently player using
-        /// </summary>
-        private string lastSaveFilename = "";
-        
+
         private List<IGameSave> gameSaves = new List<IGameSave>();
-
-        private const string FILE_EXTENSION = ".gls";
-
-        protected override void Awake()
-        {
-            base.Awake();
-            if (useEasySave)
-            {
-                defaultFilename = ES3Settings.defaultSettings.path;
-            }
-        }
 
         private void Start()
         {
             if (useAutoSave)
             {
-                InvokeRepeating(nameof(AutoSave), autoSaveInterval.MinuteToSecond(), autoSaveInterval.MinuteToSecond());
+                InvokeRepeating(nameof(Save), autoSaveInterval.MinuteToSecond(), autoSaveInterval.MinuteToSecond());
             }
+            
+            LoadCachedFile();
         }
 
         private void OnApplicationPause(bool pauseStatus)
         {
-            if (string.IsNullOrEmpty(lastSaveFilename) || !pauseStatus || !saveOnApplicationPause) return;
+            if (!saveOnApplicationPause) return;
 
-            Save(lastSaveFilename);
+            Save();
         }
 
         private void OnApplicationQuit()
         {
-            if (string.IsNullOrEmpty(lastSaveFilename) || !saveOnApplicationQuit) return;
+            if (!saveOnApplicationQuit) return;
 
-            Save(lastSaveFilename);
-        }
-
-        private void AutoSave()
-        {
-            if (string.IsNullOrEmpty(lastSaveFilename))
-            {
-                Debug.Log("Attempting auto save but no current save file is used at the moment.");
-                return;
-            }
-            
-            Save(lastSaveFilename);
-            Debug.Log("Auto Saving...");
+            Save();
         }
 
         public void Initialize(IGameSave gameSave)
@@ -78,94 +49,52 @@ namespace GameLokal.Toolkit
             gameSaves.Add(gameSave);
         }
 
-        public void Save(string saveFileName = "")
+        public void Save()
         {
-            if (string.IsNullOrEmpty(saveFileName))
-            {
-                saveFileName = defaultFilename;
-            }
+            var settings = new ES3Settings(ES3.Location.Cache);
             
-            lastSaveFilename = saveFileName;
-            
-            // Save without Easy Save 3
-            /*var wrappers = gameSaves.Select(gameSave => new JsonWrapper
-            {
-                uniqueName = gameSave.GetUniqueName(), value = JsonUtility.ToJson(gameSave.GetSaveData())
-            }).ToList();
-
-            SaveLoad.WriteFile(JsonHelper.ToJson(wrappers, true),saveFileName + FILE_EXTENSION);*/
-            
-            //Save With Easy Save 3
             foreach (var gameSave in gameSaves)
             {
                 var uniqueName = gameSave.GetUniqueName();
-                ES3.Save(uniqueName, gameSave.GetSaveData());
+                ES3.Save(uniqueName, gameSave.GetSaveData(), settings);
             }
-            
-            Debug.Log($"Game saved to {Application.persistentDataPath}/{saveFileName + FILE_EXTENSION}");
+
+            if (storeCachedFileAfterSaving)
+            {
+                StoreCachedFile();
+            }
         }
 
-        public void Load(string loadFileName = "")
+        public void StoreCachedFile()
         {
-            if (string.IsNullOrEmpty(loadFileName))
+            ES3.StoreCachedFile();
+            Debug.Log($"Game saved to {ES3Settings.defaultSettings.path}");
+        }
+
+        public void LoadCachedFile()
+        {
+            ES3.CacheFile(ES3Settings.defaultSettings.path);
+        }
+
+        public void Load()
+        {
+            if (!ES3.FileExists(ES3Settings.defaultSettings.path))
             {
-                loadFileName = defaultFilename;
+                Debug.LogWarning($"No save file is found");
+                return;
             }
 
-            if (!useEasySave)
-            {
-                if (!SaveLoad.FileExist(loadFileName + FILE_EXTENSION))
-                {
-                    Debug.LogWarning($"No save file with name {loadFileName} is found");
-                    return;
-                }
-            }
-            else
-            {
-                if (!ES3.FileExists(defaultFilename))
-                {
-                    Debug.LogWarning($"No save file with name {loadFileName} is found");
-                    return;
-                }
-            }
-            
-            
-            lastSaveFilename = loadFileName;
-            
-            // Load without Easy Save 3
-            /*
-            var json = SaveLoad.Read(loadFileName + FILE_EXTENSION);
-            var wrappers = JsonHelper.FromJson<JsonWrapper>(json);
-            foreach (var gameSave in gameSaves)
-            {
-                foreach (var generic in from wrapper in wrappers where wrapper.uniqueName == gameSave.GetUniqueName() select JsonUtility.FromJson(wrapper.value, gameSave.GetSaveDataType()))
-                {
-                    gameSave.ResetData();
-                    gameSave.OnLoad(generic);
-                }
-            }
-            */
-            
-            // Load with Easy Save 3
+            var settings = new ES3Settings(ES3.Location.Cache);
             foreach (var gameSave in gameSaves)
             {
                 var uniqueName = gameSave.GetUniqueName();
                 if(ES3.KeyExists(uniqueName))
-                    gameSave.OnLoad(ES3.Load(uniqueName));
+                    gameSave.OnLoad(ES3.Load(uniqueName, settings));
             }
             
-            Debug.Log($"Game loaded from {Application.persistentDataPath}/{loadFileName + FILE_EXTENSION}");
+            Debug.Log($"Game loaded from {ES3Settings.defaultSettings.path}");
         }
 
-        /// <summary>
-        /// Prevent auto-save until saving or loading is occured
-        /// </summary>
-        public void ResetLastSavedFilename()
-        {
-            lastSaveFilename = "";
-            Debug.Log($"Last saved filename has been cleared. Auto-save is now turned off until saving or loading is occured.");
-        }
-        
 #if UNITY_EDITOR
         [Button(ButtonSizes.Large)]
         public void OpenPersistentDataPath()
